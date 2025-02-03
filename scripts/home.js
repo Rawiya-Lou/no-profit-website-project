34790f1f26d048799e6e3fd3bcb38569133f4a15
Rawiya lou <raoiua15@gmail.com>
2025-02-03 15:12:58 +0100

    progress

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,8 @@
 document.getElementById('donation-form').addEventListener('submit', (event) =>{
+
 let isValid = false;
 const errorMessage = document.getElementById('error-message');
-const customAmount = document.getElementById('custom-Amount').value;
+const customAmount = document.getElementById('custom-amount').value;
 
 const radios = document.getElementsByName('amount');
 for(let radio of radios){
@@ -9,29 +10,23 @@ for(let radio of radios){
         isValid = true;
         break;
     }
-    if(customAmount.clicked &&customAmount === ''){
+    if(customAmount.value === ''){
         isValid = false;
-
     }
+
     if(!isValid){
         event.preventDefault(); //prevent form submission
         errorMessage.style.display = 'block';
     }else{
         errorMessage.style.display = 'none';
     }
-    customAmount.addEventListener('change', () =>{
-        customAmount.style.display = this.checked ? 'block' : 'none';
+    customAmount.addEventListener('keypress', (event) =>{
+        if(customAmount.value === ''){
+        errorMessage.style.display = 'none'; }
+        else{
+        errorMessage.style.display = 'block';
+        }
     });
 }
 });
 
-function onGooglePayLoaded() {
-    const paymentsClient = new google.payments.api.PaymentsClient({environment: 'TEST'});
-    paymentsClient.isReadyToPay({apiVersion: 2, apiVersionMinor: 0}).then(function(response) {
-        if (response.result) {
-            document.getElementById('googlePayButton').style.display = 'block';
-        }
-    }).catch(function(err) {
-        console.error("Error determining readiness: ", err);
-    });
-}
--- scripts/thanku.js
+++ scripts/thanku.js
@@ -1,24 +1,24 @@
+
 const button = document.getElementById("subButton");
 
 const overlay = document.getElementById("overlay");
 
 const thankYouMessage = document.getElementById("thankYouMessage");
 
-
-
 function closeMessageButton(){
     const closeButton = document.getElementById("closeButton");
     closeButton.addEventListener('click', () =>{
         overlay.style.display = 'none'; 
         thankYouMessage.style.display = 'none';
-        return;
+       
     });
 }
 
 
 button.addEventListener('click', () =>{
-
     overlay.style.display = 'block'; 
+    thankYouMessage.classList.add('open-message');
     thankYouMessage.style.display = 'block';
     closeMessageButton();
+   
 });
